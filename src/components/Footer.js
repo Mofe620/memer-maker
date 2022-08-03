a7955932645d83689c4db779af7991281e237032
Mofe <moflux620@gmail.com>
2022-08-03 13:10:57 +0100

    ..updated footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        <p>Made with 😃 by <a href="https://www.github.com/mofe620" target="_blank">@Mofe620</a></p>
+        <p>Made with 😃 by <a href="https://www.github.com/mofe620" target="_blank" rel="noreferrer">@Mofe620</a></p>
     </footer>
   )
 }
